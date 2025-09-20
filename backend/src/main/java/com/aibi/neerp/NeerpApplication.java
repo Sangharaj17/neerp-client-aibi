e7d90815024c64d562e3f917e205fd33b8e90914
udaythakare <udaythakare592004@gmail.com>
2025-09-20 19:44:57 +0530

    test cicd

--- backend/src/main/java/com/aibi/neerp/NeerpApplication.java
+++ backend/src/main/java/com/aibi/neerp/NeerpApplication.java
@@ -2,13 +2,22 @@ package com.aibi.neerp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class NeerpApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(NeerpApplication.class, args);
 	}
 
+	@RestController
+	public static class HelloController {
+		
+		@GetMapping("/")
+		public String hello() {
+			return "Hello World";
+		}
+	}
 }
